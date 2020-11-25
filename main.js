65179d0abdc42f084a1aaecfbb6dc9854206a2d2
carmine <carminepepiciello@gmail.com>
2020-11-25 14:52:49 +0100

    Buon pomeriggio,si comincia. Aggiunta verifica sulle immagini e trim dell'utente

--- main.js
+++ main.js
@@ -3,39 +3,43 @@ var app = new Vue ({
     data: {
         cerca: '',
         films: [],
-        lingua: ['it','br','ca','de','en','es','fr','hi','ja','ko','pt','ru','tr','zh']
+        lingua: ['it','br','ca','de','en','es','fr','hi','ja','ko','pt','ru','tr','zh'],
+        grandezza_img:'w342'
     },
     methods: {
         cerco_film() {
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: '0d032082a99de17739cf29501eb868ad',
-                    query: app.cerca,
-                    language: 'it'
-                }
-            })
-            .then((risposta) => {
-                this.films = risposta.data.results
-                console.log(this.films);
-            });
 
-            //serie tv
-            axios.get('https://api.themoviedb.org/3/search/tv', {
-                params: {
-                    api_key: '0d032082a99de17739cf29501eb868ad',
-                    query: app.cerca,
-                    language: 'it'
-                }
-            })
-            .then((risposta) => {
-                for (let i = 0; i < risposta.data.result ; i++) {
-                    let ele_corrente = risposta.data.results[i];
-                    console.log(ele_corrente)
-                    this.films.push(ele_corrente)
-                }
-            });
+            if (this.cerca.trim()) {
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: '0d032082a99de17739cf29501eb868ad',
+                        query: app.cerca,
+                        language: 'it'
+                    }
+                })
+                    .then((risposta) => {
+                        this.films = risposta.data.results
+                        console.log(this.films);
+                    });
 
-            this.cerca = '';
+                //serie tv
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        api_key: '0d032082a99de17739cf29501eb868ad',
+                        query: app.cerca,
+                        language: 'it'
+                    }
+                })
+                    .then((risposta) => {
+                        for (let i = 0; i < risposta.data.result; i++) {
+                            let ele_corrente = risposta.data.results[i];
+                            console.log(ele_corrente)
+                            this.films.push(ele_corrente)
+                        }
+                    });
+
+                this.cerca = '';
+            }
         }
     },
     mounted() {
