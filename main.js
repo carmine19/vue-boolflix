68a830a635e8a9a611b48fee767c85bcdaf7aaab
carmine <carminepepiciello@gmail.com>
2020-11-26 22:25:52 +0100

    reup,

--- main.js
+++ main.js
@@ -74,43 +74,15 @@ var app = new Vue ({
           }
         },
 
-        scrollFunction() {
-
-            let scrollpos = window.scrollY
-              const header = document.getElementById("nav")
-              const header_height = header.offsetHeight
-
-              const add_class_on_scroll = () => header.classList.add("fade-in")
-              const remove_class_on_scroll = () => header.classList.remove("fade-in")
-
-              window.addEventListener('scroll', function() {
-                scrollpos = window.scrollY;
-
-                if (scrollpos >= header_height) { add_class_on_scroll() }
-                else { remove_class_on_scroll() }
-
-                console.log(scrollpos)
-              })
-        },
 
     },
     mounted() {
         this.load = true
         this.film_base()
         this.tronco_stringa(str, num)
-        this.scrollFunction()
 
 
-    }
-
-
-
-
-
-
-
-
-
-
+    },
 
 });
+
